0b24a7a3c055c0ad0fe04a04a3b152c667eeea96
kkssbbb <03337@naver.com>
2024-10-23 14:54:36 +0900

    refactor: sendInvalidCommandMessage() 메서드 중복제거를 위한 리팩토링
    sendInvalidCommandMessage() 메서드는 유효하지않은 명령어를 사용시에 실행됨으로 모든 명령어 클래스에서 사용될것같아 중복 제거를위해 Command 인터페이스에 디폴트 메서드로 리팩토링하여 메서드 중복 제거

--- src/main/java/v1/server/command/Command.java
+++ src/main/java/v1/server/command/Command.java
@@ -2,7 +2,18 @@ package v1.server.command;
 
 import v1.server.Session;
 
+import java.io.IOException;
+
 public interface Command {
     void execute(Session session, String[] args);
     String getCommandName();
+
+
+    default void sendInvalidCommandMessage(Session session) {
+        try {
+            session.sendMessage("명령어가 유효하지 않습니다. 명령어가 맞는지 다시 확인해 주세요");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
